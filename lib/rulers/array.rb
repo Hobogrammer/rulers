10c1962dd587409d8a07dc55a205228e6e16a53f
Andre Smith <lordsilent@gmail.com>
2014-02-17 23:49:52 -0800

    Chapter one Exercise two

---
+++ lib/rulers/array.rb
@@ -0,0 +1,13 @@
+class Array
+  def sum(start = 0)
+    inject(start, &:+)
+  end
+
+  def multiply(start=0)
+    inject(start, &:*)
+  end
+
+  def divide(start=0)
+    inject(start, &:/)
+  end
+end
