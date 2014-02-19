eff727becc5454fc16a6e70f5d9f3702de65722b
Andre Smith <andrepsmithjr@gmail.com>
2014-02-19 00:18:57 -0800

    Chapter 2 Exercise 3

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -8,6 +8,10 @@ module Rulers
         return [404, {'Content-Type'=> 'text/html'}, []]
       end
 
+      if env['PATH_INFO'] == '/'
+        env['PATH_INFO'] = '/homes/index'
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       begin
