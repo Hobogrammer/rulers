50a94ccf680c224c88a876bce8739798df340df7
Andre Smith <lordsilent@gmail.com>
2014-02-23 17:31:27 -0800

    Chapter 4

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,6 +2,7 @@ require "rulers/version"
 require "rulers/routing"
 require "rulers/util"
 require "rulers/dependencies"
+require "rulers/controller"
 
 module Rulers
   class Application
@@ -22,16 +23,6 @@ module Rulers
         return [500, {'Content-Type' => 'text/html'}, ['Something went wrong.']]
       end
       [200, {'Content-Type' => 'text/html'},[text]]
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
     end
   end
 end
