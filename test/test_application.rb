ab004d30e1752e31a11b80f5123d1f801eeecb6e
Andre Smith <lordsilent@gmail.com>
2014-02-23 18:23:18 -0800

    Update tests for Chap 4 Exercise 1

--- test/test_application.rb
+++ test/test_application.rb
@@ -1,6 +1,15 @@
 require_relative "test_helper"
 
+class TestController < Rulers::Controller
+  def index
+    "Hello"
+  end
+end
+
 class TestApp < Rulers::Application
+  def get_controller_and_action(env)
+    [TestController, "index"]
+  end
 end
 
 class RulersAppTest < Test::Unit::TestCase
@@ -11,7 +20,7 @@ class RulersAppTest < Test::Unit::TestCase
   end
 
   def test_request
-    get "/"
+    get "/example/route"
 
     assert last_response.ok?
     body = last_response.body
@@ -19,7 +28,7 @@ class RulersAppTest < Test::Unit::TestCase
   end
 
   def test_content
-    get "/"
+    get "/example/route"
 
     assert last_response.content_type =='text/html'
   end
